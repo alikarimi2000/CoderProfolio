cf7317f879b74a48db7a0fbd918b8f25f3002a56
alikarimi2000 <karimyai46@gmail.com>
2023-07-24 12:55:36 +0330

    Add Project IProjectApplication and Project Repository

--- CD.Domin/ProjectAgg/IProjectRepository.cs
+++ CD.Domin/ProjectAgg/IProjectRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CD.ApplicationContracts.Project;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,8 @@ namespace CD.Domain.ProjectAgg
     public interface IProjectRepository
     {
         void Creat(Project project);
+        void SaveChange();
+        EditProject GetDetails(int id);
         Project Get(int Id);
 
     }
